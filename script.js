58d0d6571ffaa08d03713b0750448b495ad016d9
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-06-05 11:09:53 -0400

    Added Pitchers

--- script.js
+++ script.js
@@ -59,13 +59,13 @@ function init(){
                 </div>
                 <div class="card-body">
                     
-                    <h6>${items[item].away_team_name}</h6><p>${items[item].away_win}-${items[item].away_loss}</p>
-                    <h6>${items[item].home_team_name}</h6><p>${items[item].home_win}-${items[item].home_loss}</p>
+                    <h6>${items[item].away_team_name}</br>${items[item].away_win}-${items[item].away_loss}</h6>
+                    <h6>${items[item].home_team_name}</br>${items[item].home_win}-${items[item].home_loss}</h6>
+                    <hr>
+
+                    <h6>${items[item].away_probable_pitcher.last}, ${items[item].away_probable_pitcher.first}</br>${items[item].away_win}-${items[item].away_loss}</h6>
+                    <h6>${items[item].home_probable_pitcher.last}, ${items[item].home_probable_pitcher.first}</br>${items[item].home_win}-${items[item].home_loss}</h6>
                     
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <small class="text-muted">9 mins</small>
-                    </div>
                   </div>
                 </div>
               </div>`
