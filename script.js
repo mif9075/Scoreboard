dc436c247372dd7fa0110ea7636836d03088a468
Miguel A. Fernandez <miguel.fernandez@codeimmersives.com>
2019-06-05 10:33:11 -0400

    Added time of games

--- script.js
+++ script.js
@@ -52,9 +52,11 @@ function init(){
 
         for (let item in items) {
             if (items.hasOwnProperty(item)) {
+                console.log(items[item].time);
                 cards += `<div class="col-md-4">
-                <div class="card mb-4 shadow-sm">
-                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <div class="card-header">
+                <h4 class="my-0 font-weight-normal">${items[item].time} ${items[item].ampm} ${items[item].home_time_zone}</h4>
+                </div>
                   <div class="card-body">
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <div class="d-flex justify-content-between align-items-center">
